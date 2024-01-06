ef00308f9c0e5cad27b9a11660f0d645efa2c80f
Lennice Bolton <lennicebolton03@gmail.com>
2024-01-06 07:13:48 -0500

    adding the icons.js file to the test branch

--- frontend/src/utils/icons.js
+++ frontend/src/utils/icons.js
@@ -1,4 +1,5 @@
-export const dashboard = <i className ="fa-solid fa-house"></i>
-export const dashboard = <i className ="fa-solid fa-house"></i>
-export const dashboard = <i className ="fa-solid fa-house"></i>
-export const dashboard = <i className ="fa-solid fa-house"></i>
+export const dashboard = <i className ="fa-solid fa-chart-line"></i>
+export const transactions = <i class="fa-solid fa-credit-card"></i>
+export const categories =  <i class="fa-solid fa-tag"></i>
+export const accounts = <i class="fa-solid fa-wallet"></i>
+  
