2f33f1f75ec2d4dc4170ab61d693e47b29814947
Lennice Bolton <lennicebolton03@gmail.com>
2024-01-06 07:05:21 -0500

    Icons, and Fonts
    -In the process of adding icons and fonts

---
+++ frontend/src/utils/menuAssets.js
@@ -0,0 +1,23 @@
+export const menuItems = [
+    {
+        id: 1,
+        title: 'Dashboard',
+        icon: '',
+        link: '/dashboard'
+    },
+    {
+        id: 2,
+        title: "View Transactions",
+        icon: transactions,
+        link: "/dashboard",
+    },
+    {
+        id: 3,
+        title: "Income",
+        icon: trend,
+        link: "/dashboard",
+    },
+    {
+        
+    }
+]
