63c2f92a5d3b2469b04a56f6d2edb0d3a8c8ffcd
Lennice Bolton <lennicebolton03@gmail.com>
2024-01-08 23:20:56 -0500

    In the process of stylis the navigation panel.

--- frontend/src/Components/orb/Navigation.js
+++ frontend/src/Components/orb/Navigation.js
@@ -7,26 +7,58 @@ import { signout } from '../../utils/icons'
 
 
 const NavStyled = styled.nav`
-padding: 2rem 1.5rem;
-width: 374px;
-height: 100%;
-background: rgba(252,246,249,0.78);
-border: 3px solid #FFFFFF;
-backdrop-filter: blur(4.5px);
-border-radius: 32px;
-display: flex;
-flex-direction: column;
-justify-content: space-between;
-gap: 2rem;
+    padding: 2rem 1.5rem;
+    width: 374px;
+    height: 100%;
+    background: rgba(252,246,249,0.78);
+    border: 3px solid #FFFFFF;
+    backdrop-filter: blur(4.5px);
+    border-radius: 32px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    gap: 2rem;
+    .user-con{
+        height: 200px;
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+        img{
+            width: 100px;
+            height: 100px;
+            border-radius: 5%;
+            object-fit: cover;
+            background-color: #fcf6f9;
+            border: 2px solid #FFFFFF;
+            padding: .2rem;
+            box-shadow: 0px 1px 17px rgba(0,0,0,0.06);
+        }
+        h2{
+            color: rgba(34, 34, 96, 1);
+        }
+        p{
+            color: rgba(34, 34, 96, .6)
+        }
+        .menu-items{
+            flex: 1;
+            display: flex;
+            flex-direction:column;
+            li{
+                display: grid;
+                grid-template-columns: 40px auto;
+
+            }
+        }
+    }
+    
 
 `;
-
 function Navigation() {
     console.log('menuItems:', menuItems);
 
     return (
         <NavStyled>
-            <div className='user'>
+            <div className='user-con'>
                 <img src={avatar} alt="" />
                 <div className='text'>
                     <h2>Lennice</h2>
