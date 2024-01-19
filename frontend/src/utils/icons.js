264843ee052d19cf36142882cc699523295c3670
Lennice Bolton <lennicebolton03@gmail.com>
2024-01-19 12:30:24 -0500

    Revert "Merge pull request #3 from DayBreak3681/testing-branch"
    This reverts commit 2be2d2538622d3068475af8405bd80cf281dd120, reversing
    changes made to c1e33b164eae750ca97837bf0213e58fd24b6cb7.

--- frontend/src/utils/icons.js
+++ frontend/src/utils/icons.js
@@ -1,5 +1,4 @@
-export const dashboard = <i className ="fa-solid fa-chart-line"></i>
-export const transactions = <i class="fa-solid fa-credit-card"></i>
-export const categories =  <i class="fa-solid fa-tag"></i>
-export const accounts = <i class="fa-solid fa-wallet"></i>
-  
+export const dashboard = <i className ="fa-solid fa-house"></i>
+export const dashboard = <i className ="fa-solid fa-house"></i>
+export const dashboard = <i className ="fa-solid fa-house"></i>
+export const dashboard = <i className ="fa-solid fa-house"></i>
